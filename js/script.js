61df058a5c976f9daba90d2a2dd02a9eb47d8d28
hyunukkang <hyunukiop@gmailc.om>
2021-06-18 10:58:09 +0900

    0618

--- js/script.js
+++ js/script.js
@@ -1,15 +1,40 @@
 
 
 $(function(){
-        var $skillsTitle = $('.skills__title')
-        var $skillsDesc = $('.skills__desc');
-        var $skillsIcon = $('.skills_icon');
-        
-        $skillsDesc.hide();
-        $skillsTitle.click(function(){
-        $skillsDesc.stop().slideUp();
-        $(this).siblings().stop().slideToggle();
-        $(this).children().toggleClass('visible');
-        // $skillsIcon.toggleClass('visible'); /* 사라졌다 / 보였다 */
+    // >>>>>>>>>>>>>>>>>>> tab
+    var $subTitleTab = $('.sub-title__tab');
+    var $aboutKeyword = $('.about__keyword');
+    $aboutKeyword.eq(1).hide();
+    $subTitleTab.click(function(e){
+        e.preventDefault();
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+        var idx = $(this).index();
+        $aboutKeyword.hide();
+        $aboutKeyword.eq(idx).show();
+    })
+    // >>>>>>>>>>>>>>>>>>>>count
+    $('.stoudyCount, .publCount, .designCount, .blogCount').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 3000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+    // skills accordion
+    var $skillsTitle = $('.skills__title')
+    var $skillsDesc = $('.skills__desc');
+    var $skillsIcon = $('.skills_icon');
+    
+    $skillsDesc.hide();
+    $skillsTitle.click(function(){
+    $skillsDesc.stop().slideUp();
+    $(this).siblings().stop().slideToggle();
+    $(this).children().toggleClass('visible');
+    // $skillsIcon.toggleClass('visible'); /* 사라졌다 / 보였다 */
     })
 })
