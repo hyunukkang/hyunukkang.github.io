18e401118fceda4171b82e6fb80c742c3c35dfa9
hyunuk kang <hyunuk.green@gmail.com>
2021-06-22 01:30:50 +0900

    publishing

--- js/script.js
+++ js/script.js
@@ -108,6 +108,38 @@ $(function(){
     //     // autoplaySpeed: 2000,
     // });
     
+    // >>>>>>>> publishing slider
+    $('.slider__custom').slick({
+        dots: true,
+        // infinite: true,
+        // speed: 300,
+        // autoplay: true,
+        // autoplaySpeed: 3000,
+        centerMode: true,
+        // centerPadding: '20px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
     // portfolio design modal
     $('.design__btn').click(function(e){
         e.preventDefault();
